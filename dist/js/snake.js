78d867051fdb5d0c16b4d9fa9955e046a377e4df
nielsvanrijn <niels@divisionvitrum.com>
2017-05-24 16:50:18 +0200

    general update

--- dist/js/snake.js
+++ dist/js/snake.js
@@ -1,3 +1,97 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Piece = (function () {
+    function Piece(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 30;
+        this.height = 30;
+    }
+    Piece.prototype.moveToPoint = function (p) {
+        if (this.tail != null) {
+            this.tail.moveToPoint(this);
+        }
+        this.x = p.x;
+        this.y = p.y;
+    };
+    return Piece;
+}());
+var Food = (function (_super) {
+    __extends(Food, _super);
+    function Food(game) {
+        _super.call(this, 0, 0);
+        this.game = game;
+    }
+    return Food;
+}(Piece));
+var Snek = (function (_super) {
+    __extends(Snek, _super);
+    function Snek(game) {
+        _super.call(this, 0, 0);
+        this.snekColor = 0;
+        this.game = game;
+    }
+    Snek.prototype.restart = function () {
+        this.snekHead = new Piece(60, 30);
+        this.snekHead.tail = new Piece(30, 30);
+        this.snekHead.tail.tail = new Piece(0, 30);
+        this.direction = Direction.Right;
+    };
+    Snek.prototype.move = function () {
+        var xNew = this.snekHead.x;
+        var yNew = this.snekHead.y;
+        switch (this.direction) {
+            case Direction.Left:
+                xNew -= this.game.tileSize;
+                break;
+            case Direction.Right:
+                xNew += this.game.tileSize;
+                break;
+            case Direction.Up:
+                yNew -= this.game.tileSize;
+                break;
+            case Direction.Down:
+                yNew += this.game.tileSize;
+                break;
+            default:
+                break;
+        }
+        if (xNew == this.game.food.x &&
+            yNew == this.game.food.y) {
+            this.eat();
+        }
+        else {
+            this.snekHead.tail.moveToPoint(this.snekHead);
+            this.snekHead.x = xNew;
+            this.snekHead.y = yNew;
+        }
+    };
+    Snek.prototype.moveLeft = function () {
+        if (this.direction != Direction.Right)
+            this.direction = Direction.Left;
+    };
+    Snek.prototype.moveRight = function () {
+        if (this.direction != Direction.Left)
+            this.direction = Direction.Right;
+    };
+    Snek.prototype.moveUp = function () {
+        if (this.direction != Direction.Down)
+            this.direction = Direction.Up;
+    };
+    Snek.prototype.moveDown = function () {
+        if (this.direction != Direction.Up)
+            this.direction = Direction.Down;
+    };
+    Snek.prototype.eat = function () {
+        this.game.food.tail = this.snekHead;
+        this.snekHead = this.game.food;
+        this.game.placeFood();
+    };
+    return Snek;
+}(Piece));
 var Direction;
 (function (Direction) {
     Direction[Direction["Left"] = 0] = "Left";
@@ -7,119 +101,32 @@ var Direction;
 })(Direction || (Direction = {}));
 var Game = (function () {
     function Game(canvas) {
-        this.snekColor = 0;
+        var _this = this;
+        this.pauseDuration = 200;
+        window.addEventListener("keydown", function (e) { return _this.keyboardListener(e); });
         this.canvas = canvas;
         this.canvas.width = 600;
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         this.tileSize = 30;
+        this.snek = new Snek(this);
+        this.food = new Food(this);
+        this.start();
     }
     Game.prototype.start = function () {
         this.restart();
-        setInterval(function () { return game.loop(); }, 100);
+        setInterval(function () { return game.loop(); }, this.pauseDuration);
     };
     Game.prototype.restart = function () {
-        this.snekeHead = new Point(60, 30);
-        this.snekeHead.tail = new Point(30, 30);
-        this.snekeHead.tail.tail = new Point(0, 30);
-        this.direction = Direction.Right;
+        this.snek.restart();
         this.placeFood();
     };
     Game.prototype.loop = function () {
-        this.move();
+        this.snek.move();
         if (this.gameOver()) {
             this.restart();
         }
         this.draw();
-    };
-    Game.prototype.snekOutsideBounds = function () {
-        return (this.snekeHead.x < 0 ||
-            this.snekeHead.y < 0 ||
-            this.snekeHead.x + this.snekeHead.width > this.canvas.width ||
-            this.snekeHead.y + this.snekeHead.height > this.canvas.height);
-    };
-    Game.prototype.snekTouchingItself = function () {
-        var p = this.snekeHead.tail;
-        while (p != null) {
-            if (p.x == this.snekeHead.x &&
-                p.y == this.snekeHead.y)
-                return true;
-            p = p.tail;
-        }
-        return false;
-    };
-    Game.prototype.gameOver = function () {
-        return this.snekOutsideBounds() || this.snekTouchingItself();
-    };
-    Game.prototype.draw = function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        var p = this.snekeHead;
-        while (p != null) {
-            this.context.fillStyle = "hsl(" + this.snekColor + ", 100%, 50%)";
-            if (this.snekColor < 360) {
-                this.snekColor = this.snekColor + 1;
-            }
-            else {
-                this.snekColor = this.snekColor - 360;
-            }
-            this.context.fillRect(p.x, p.y, p.width - 1, p.height - 1);
-            p = p.tail;
-            this.context.fillStyle = "hsl(" + (360 - this.snekColor) + ", 100%, 50%)";
-        }
-        this.context.fillRect(this.food.x, this.food.y, this.food.width - 1, this.food.height - 1);
-    };
-    Game.prototype.move = function () {
-        var xNew = this.snekeHead.x;
-        var yNew = this.snekeHead.y;
-        switch (this.direction) {
-            case Direction.Left:
-                xNew -= this.tileSize;
-                break;
-            case Direction.Right:
-                xNew += this.tileSize;
-                break;
-            case Direction.Up:
-                yNew -= this.tileSize;
-                break;
-            case Direction.Down:
-                yNew += this.tileSize;
-                break;
-            default:
-                break;
-        }
-        if (xNew == this.food.x &&
-            yNew == this.food.y) {
-            this.eat();
-        }
-        else {
-            this.snekeHead.tail.moveToPoint(this.snekeHead);
-            this.snekeHead.x = xNew;
-            this.snekeHead.y = yNew;
-        }
-    };
-    Game.prototype.moveLeft = function () {
-        if (this.direction != Direction.Right)
-            this.direction = Direction.Left;
-    };
-    Game.prototype.moveRight = function () {
-        if (this.direction != Direction.Left)
-            this.direction = Direction.Right;
-    };
-    Game.prototype.moveUp = function () {
-        if (this.direction != Direction.Down)
-            this.direction = Direction.Up;
-    };
-    Game.prototype.moveDown = function () {
-        if (this.direction != Direction.Up)
-            this.direction = Direction.Down;
-    };
-    Game.prototype.eat = function () {
-        this.food.tail = this.snekeHead;
-        this.snekeHead = this.food;
-        this.placeFood();
-    };
-    Game.prototype.randomInt = function (upperRange, lowerRange) {
-        return Math.round((Math.floor(Math.random() * (upperRange - lowerRange + 1)) + lowerRange));
     };
     Game.prototype.placeFood = function () {
         var noOfTiles = (this.canvas.width / this.tileSize) * (this.canvas.height / this.tileSize);
@@ -130,68 +137,93 @@ var Game = (function () {
             if (i == 799) {
                 var b = i;
             }
-            a[i] = new Point(x, y);
+            a[i] = new Piece(x, y);
         }
         var snekParts = new Array();
-        var p = this.snekeHead;
+        var p = this.snek.snekHead;
         while (p != null) {
             snekParts.push(p);
             p = p.tail;
         }
         var validPoints = new Array();
         for (var i = 0; i < a.length; i++) {
-            if (!this.pointInArray(a[i], snekParts))
+            if (!Util.pointInArray(a[i], snekParts))
                 validPoints.push(a[i]);
         }
-        var newPointIndex = this.randomInt(validPoints.length - 1, 0);
+        var newPointIndex = Util.randomInt(validPoints.length - 1, 0);
         this.food = validPoints[newPointIndex];
     };
-    Game.prototype.pointInArray = function (p, pointArray) {
-        for (var i = 0; i < pointArray.length; i++) {
-            var pointAtIndex = pointArray[i];
-            if (pointAtIndex.x == p.x && pointAtIndex.y == p.y)
+    Game.prototype.gameOver = function () {
+        return Util.snekOutsideBounds(this.snek, this.canvas) || Util.snekTouchingItself(this.snek);
+    };
+    Game.prototype.draw = function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        var p = this.snek.snekHead;
+        while (p != null) {
+            this.context.fillStyle = "hsl(" + this.snek.snekColor + ", 100%, 50%)";
+            if (this.snek.snekColor < 360) {
+                this.snek.snekColor = this.snek.snekColor + 0.5;
+            }
+            else {
+                this.snek.snekColor = this.snek.snekColor - 360;
+            }
+            this.context.fillRect(p.x, p.y, p.width - 1, p.height - 1);
+            p = p.tail;
+            this.context.fillStyle = "hsl(" + (360 - this.snek.snekColor) + ", 100%, 50%)";
+        }
+        this.context.fillRect(this.food.x, this.food.y, this.food.width - 1, this.food.height - 1);
+    };
+    Game.prototype.keyboardListener = function (e) {
+        if (e.keyCode == 37) {
+            this.snek.moveLeft();
+        }
+        else if (e.keyCode == 39) {
+            this.snek.moveRight();
+        }
+        else if (e.keyCode == 38) {
+            this.snek.moveUp();
+        }
+        else if (e.keyCode == 40) {
+            this.snek.moveDown();
+        }
+    };
+    return Game;
+}());
+var game;
+window.onload = function () {
+    var el = document.getElementById('game-canvas');
+    game = new Game(el);
+};
+var Util = (function () {
+    function Util() {
+    }
+    Util.randomInt = function (upperRange, lowerRange) {
+        return Math.round((Math.floor(Math.random() * (upperRange - lowerRange + 1)) + lowerRange));
+    };
+    Util.pointInArray = function (p, pieceArray) {
+        for (var i = 0; i < pieceArray.length; i++) {
+            var pieceAtIndex = pieceArray[i];
+            if (pieceAtIndex.x == p.x && pieceAtIndex.y == p.y)
                 return true;
         }
         return false;
     };
-    return Game;
-}());
-function keyboardListener(e) {
-    if (e.keyCode == 37) {
-        game.moveLeft();
-    }
-    else if (e.keyCode == 39) {
-        game.moveRight();
-    }
-    else if (e.keyCode == 38) {
-        game.moveUp();
-    }
-    else if (e.keyCode == 40) {
-        game.moveDown();
-    }
-}
-;
-var game;
-window.onload = function () {
-    document.onkeydown = keyboardListener;
-    var el = document.getElementById('game-canvas');
-    game = new Game(el);
-    game.start();
-};
-var Point = (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-        this.width = 30;
-        this.height = 30;
-    }
-    Point.prototype.moveToPoint = function (p) {
-        if (this.tail != null) {
-            this.tail.moveToPoint(this);
-        }
-        this.x = p.x;
-        this.y = p.y;
+    Util.snekOutsideBounds = function (snek, canvas) {
+        return (snek.snekHead.x < 0 ||
+            snek.snekHead.y < 0 ||
+            snek.snekHead.x + snek.snekHead.width > canvas.width ||
+            snek.snekHead.y + snek.snekHead.height > canvas.height);
     };
-    return Point;
+    Util.snekTouchingItself = function (snek) {
+        var p = snek.snekHead.tail;
+        while (p != null) {
+            if (p.x == snek.snekHead.x &&
+                p.y == snek.snekHead.y)
+                return true;
+            p = p.tail;
+        }
+        return false;
+    };
+    return Util;
 }());
 //# sourceMappingURL=snake.js.map
